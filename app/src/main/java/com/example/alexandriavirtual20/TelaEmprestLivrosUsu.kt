43e6aa78b55b2663ef35d4a06ba2c5b081487da1
Dylan <dillanmedeiros404@gmail.com>
2025-10-02 18:49:18 -0300

    Alteração tela Menu de Emprestimo e Tela Emprestimo de Livros

--- app/src/main/java/com/example/alexandriavirtual20/TelaEmprestLivrosUsu.kt
+++ app/src/main/java/com/example/alexandriavirtual20/TelaEmprestLivrosUsu.kt
@@ -1,15 +1,58 @@
 package com.example.alexandriavirtual20
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class TelaEmprestLivrosUsu : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.tela_emprest_livros_usu)
+        val spinnerAutor: Spinner = findViewById(R.id.spinnerAutor)
+        val spinnerGenero: Spinner = findViewById(R.id.spinnerGenero)
+        val spinnerMaisPopulares: Spinner = findViewById(R.id.spinnerMaisPopulares)
+        val spinnerLancamento: Spinner = findViewById(R.id.spinnerLancamento)
+
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.autores_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerAutor.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.generos_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerGenero.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.populares_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerMaisPopulares.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.lancamentos_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerLancamento.adapter = adapter
+        }
     }
 }
