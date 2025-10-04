2c0165eb0976a2f36727bb592ed23b20b74973a2
NicollyFeitosa <nicollybpcs@gmail.com>
2025-10-04 20:59:38 -0300

    Tela Levantamento Bibliográfico concluída

--- app/src/main/java/com/example/alexandriavirtual20/TelaLevantBibliogUsu.kt
+++ app/src/main/java/com/example/alexandriavirtual20/TelaLevantBibliogUsu.kt
@@ -1,15 +1,22 @@
 package com.example.alexandriavirtual20
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
 
 class TelaLevantBibliogUsu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.tela_levant_bibliog_usu)
+
+        val botaoVoltar = findViewById<ImageButton>(R.id.botaoVoltar)
+        botaoVoltar.setOnClickListener{
+            val intent = Intent(this, TelaInicioUsu::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
