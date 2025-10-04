e6970f660ff24431861efe343d7d1d289b425538
Dylan <dillanmedeiros404@gmail.com>
2025-10-04 15:09:01 -0300

    Finalização das telas de Menu de Empréstimo e Empréstimo de Livros

--- app/src/main/java/com/example/alexandriavirtual20/TelaEmprestLivrosUsu.kt
+++ app/src/main/java/com/example/alexandriavirtual20/TelaEmprestLivrosUsu.kt
@@ -1,12 +1,12 @@
 package com.example.alexandriavirtual20
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.*
+import kotlin.jvm.java
 
 class TelaEmprestLivrosUsu : AppCompatActivity() {
 
@@ -17,6 +17,16 @@ class TelaEmprestLivrosUsu : AppCompatActivity() {
         val spinnerGenero: Spinner = findViewById(R.id.spinnerGenero)
         val spinnerMaisPopulares: Spinner = findViewById(R.id.spinnerMaisPopulares)
         val spinnerLancamento: Spinner = findViewById(R.id.spinnerLancamento)
+        val titulo1: TextView = findViewById(R.id.txtTitulo1)
+        val titulo2: TextView = findViewById(R.id.txtTitulo2)
+
+        val btnAvaliacoes1: TextView = findViewById(R.id.btnAvaliacoes1)
+        val btnAvaliacoes2: TextView = findViewById(R.id.btnAvaliacoes2)
+
+        val check1: CheckBox = findViewById(R.id.checkLivro1)
+        val check2: CheckBox = findViewById(R.id.checkLivro2)
+
+        val btnConfirmar: Button = findViewById(R.id.btnConfirmar)
 
 
         ArrayAdapter.createFromResource(
@@ -54,5 +64,36 @@ class TelaEmprestLivrosUsu : AppCompatActivity() {
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinnerLancamento.adapter = adapter
         }
+        titulo1.setOnClickListener {
+            val intent = Intent(this, TelaInfoLivroUsu::class.java)
+            startActivity(intent)
+        }
+
+        titulo2.setOnClickListener {
+            val intent = Intent(this, TelaInfoLivroUsu::class.java)
+            startActivity(intent)
+        }
+
+        // Clique em avaliações → abre tela de avaliações
+        btnAvaliacoes1.setOnClickListener {
+            val intent = Intent(this, TelaAvaliacoesUsu::class.java)
+            startActivity(intent)
+        }
+
+        btnAvaliacoes2.setOnClickListener {
+            val intent = Intent(this, TelaAvaliacoesUsu::class.java)
+            startActivity(intent)
+        }
+
+        // Botão confirmar → mostra os livros selecionados
+        btnConfirmar.setOnClickListener {
+            val selecionados = mutableListOf<String>()
+
+            if (check1.isChecked) selecionados.add("Ciência da Computação e Tecnologias Digitais")
+            if (check2.isChecked) selecionados.add("Java como Programar")
+
+            Toast.makeText(this, "Selecionados: $selecionados", Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
