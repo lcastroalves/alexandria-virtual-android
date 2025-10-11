dcdb9e29fce6bfa91542938b5a483f12baee2093
NicollyFeitosa <nicollybpcs@gmail.com>
2025-10-11 19:58:34 -0300

    Tela de redefinição de senha

--- app/src/main/java/com/example/alexandriavirtual20/TelaRedefinirSenha.kt
+++ app/src/main/java/com/example/alexandriavirtual20/TelaRedefinirSenha.kt
@@ -1,15 +1,25 @@
 package com.example.alexandriavirtual20
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class TelaRedefinirSenha : AppCompatActivity() {
+
+    private lateinit var btnVoltar : ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.tela_redefinir_senha)
+
+        btnVoltar = findViewById(R.id.botaoVoltar)
+        btnVoltar.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
