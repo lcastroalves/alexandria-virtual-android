07cc1182bf5c6e1a89977adcb15de3139fa0e521
NicollyFeitosa <nicollybpcs@gmail.com>
2025-10-11 17:09:14 -0300

    Lógica da tela AMain melhorada

--- app/src/main/java/com/example/alexandriavirtual20/AMain.kt
+++ app/src/main/java/com/example/alexandriavirtual20/AMain.kt
@@ -1,37 +1,52 @@
 package com.example.alexandriavirtual20
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.example.alexandriavirtual20.databinding.AMainBinding
-import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AMain : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    lateinit var bottomNavigationView: BottomNavigationView
 
-        replaceFragment(TelaInicioUsu())
-
-        bottomNavigationView.setOnItemSelectedListener( { menuItem: MenuItem? ->
-            val id = menuItem!!.getItemId()
-            if (id == R.id.inicio) {
-                replaceFragment(TelaInicioUsu())
-            } else if (id == R.id.emprestimo) {
-            } else if (id == R.id.chatbot) {
-            } else if (id == R.id.reserva) {
-            } else if (id == R.id.perfil) {
-                replaceFragment(TelaPerfilUsu())
-            }
-            true
-        })
-    }
 
     private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = getSupportFragmentManager()
+        val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frameLayout, fragment)
         fragmentTransaction.commit()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        replaceFragment(TelaInicioUsu())
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.inicio -> {
+                    replaceFragment(TelaInicioUsu())
+                    true
+                }
+                R.id.emprestimo -> {
+                    //replaceFragment(TelaEmprestimoUsu())
+                    true
+                }
+                R.id.chatbot -> {
+                    //replaceFragment(TelaChatbotUsu())
+                    true
+                }
+                R.id.reserva -> {
+                    //replaceFragment(TelaReservaUsu())
+                    true
+                }
+                R.id.perfil -> {
+                    replaceFragment(TelaPerfilUsu())
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
