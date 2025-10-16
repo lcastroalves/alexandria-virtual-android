0226c45f59e6c41baf5ea5923cd6facb960a9a55
NicollyFeitosa <nicollybpcs@gmail.com>
2025-10-16 19:37:09 -0300

    Transformando a telaInicioAdm em fragment e lógica do bottonNavegationView da AdmAMain

--- app/src/main/java/com/example/alexandriavirtual20/AdmAMain.kt
+++ app/src/main/java/com/example/alexandriavirtual20/AdmAMain.kt
@@ -20,7 +20,35 @@ class AdmAMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.adm_a_main)
-        //replaceFragment(AdmTelaInicio())
+        replaceFragment(AdmTelaInicio())
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.inicio -> {
+                    replaceFragment(AdmTelaInicio())
+                    true
+                }
+                R.id.emprestimo -> {
+                    replaceFragment(AdmTelaSolicPend())
+                    true
+                }
+                R.id.chatbot -> {
+                    replaceFragment(TelaChatbotUsu())
+                    true
+                }
+                R.id.reserva -> {
+                    replaceFragment(AdmTelaCabReserv())
+                    true
+                }
+                R.id.perfil -> {
+                    replaceFragment(AdmPerfil())
+                    true
+                }
+                else -> false
+            }
+        }
 
     }
 
