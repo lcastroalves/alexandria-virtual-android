0d54c5c254c28dcc98d8f7f05d0cd349d6b9d007
FelipeBarrosoVieiraMaciel <lipebvm@gmail.com>
2025-10-17 22:48:22 -0300

    Algumas correções de telas anteriores e tela adm eventos prototipada

--- app/src/main/java/com/example/alexandriavirtual20/TelaEventosFuturUsu.kt
+++ app/src/main/java/com/example/alexandriavirtual20/TelaEventosFuturUsu.kt
@@ -1,15 +1,26 @@
 package com.example.alexandriavirtual20
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class TelaEventosFuturUsu : AppCompatActivity() {
+    private lateinit var btnVoltar: Button
+    private lateinit var dataEvento: TextView
+    private lateinit var tituloEvento: TextView
+    private lateinit var descricaoEvento: TextView
+    private lateinit var imagemEvento: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.tela_eventos_futuros_usu)
+        
+        btnVoltar = findViewById(R.id.botaoVoltar)
+
     }
 }
