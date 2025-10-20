10f9e919b614e36cad2cc0d6f6622fe3231c1c2b
FelipeBarrosoVieiraMaciel <lipebvm@gmail.com>
2025-10-20 13:56:12 -0300

    Corrigido o problema de background da tela admEventos

--- app/src/main/java/com/example/alexandriavirtual20/AdmTelaEventos.kt
+++ app/src/main/java/com/example/alexandriavirtual20/AdmTelaEventos.kt
@@ -1,15 +1,19 @@
 package com.example.alexandriavirtual20
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class AdmTelaEventos : AppCompatActivity() {
+    private lateinit var btnVoltar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.adm_tela_eventos)
+
+        btnVoltar = findViewById(R.id.botaoVoltar2)
     }
 }
