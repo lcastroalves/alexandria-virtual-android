a0b09228fc6232b7fedfb97f1f26dfaeafc9afd4
FelipeBarrosoVieiraMaciel <lipebvm@gmail.com>
2025-10-23 19:03:05 -0300

    Tela info Evento finalizada, vários itens adicionados para fins visuais.

--- app/src/main/java/com/example/alexandriavirtual20/adapter/ListaEventoAdapter.kt
+++ app/src/main/java/com/example/alexandriavirtual20/adapter/ListaEventoAdapter.kt
@@ -1,60 +1,53 @@
-//package com.example.alexandriavirtual20.adapter
-//
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.ImageButton
-//import android.widget.ImageView
-//import android.widget.TextView
-//import androidx.recyclerview.widget.RecyclerView
-//import com.example.alexandriavirtual20.R
-//import com.example.alexandriavirtual20.model.Evento
-//
-//class ListaEventoAdapter (
-//    private val eventos: MutableList<Evento>
-//) :
-//    RecyclerView.Adapter<NotificacaoAdapter.ViewHolder>() {
-//
-//    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-//        val imagem: ImageView = view.findViewById(R.id.imagemNotificacao)
-//        val nome: TextView = view.findViewById(R.id.nome)
-//        val data: TextView = view.findViewById(R.id.data)
-//        val mensagem: TextView = view.findViewById(R.id.mensagem)
-//        val btnExcluir: ImageButton = view.findViewById(R.id.btnExcluir)
-//    }
-//
-//    override fun onCreateViewHolder(
-//        parent: ViewGroup,
-//        viewType: Int
-//    ): ViewHolder {
-//        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.u_cardview_evento_da_lista, parent, false))
-//    }
-//
-//    override fun onBindViewHolder(holder: EventoAdapter.ViewHolder, position: Int) {
-//        val evento = eventos[position]
-//
-//        holder.imagem.setImageResource(evento.imagem)
-//        holder.nome.text = evento.nome
-//        holder.tipo.text = evento.tipo
-//        holder.data.text = .data
-//        holder.mensagem.text = notificacao.mensagem
-//
-//        holder.btnExcluir.setOnClickListener {
-//            onExcluirClick?.invoke(notificacao)
-//        }
-//    }
-//
-//    fun removerNotificacao(notificacao: Notificacao) {
-//        val position = notificacoes.indexOf(notificacao)
-//        if (position > -1) {
-//            notificacoes.removeAt(position)
-//            notifyItemRemoved(position)
-//            notifyItemRangeChanged(position, itemCount)
-//        }
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return notificacoes.size
-//    }
-//
-//}
+package com.example.alexandriavirtual20.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.alexandriavirtual20.R
+import com.example.alexandriavirtual20.model.Evento
+
+class ListaEventoAdapter (
+    private val eventos: MutableList<Evento>,
+    private val onClick: (Evento) -> Unit
+) :
+    RecyclerView.Adapter<ListaEventoAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val imagem: ImageView = view.findViewById(R.id.imagemEventoLista)
+        val nome: TextView = view.findViewById(R.id.nome)
+        val data: TextView = view.findViewById(R.id.data)
+        val descricao: TextView = view.findViewById(R.id.descricaoBreve)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.u_cardview_evento_da_lista, parent, false)
+
+        val viewHolder = ViewHolder(view)
+
+        viewHolder.itemView.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            onClick(eventos[position])
+        }
+
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: ListaEventoAdapter.ViewHolder, position: Int) {
+        val evento = eventos[position]
+
+        holder.imagem.setImageResource(evento.imagem)
+        holder.nome.text = evento.nome
+        holder.data.text = evento.data
+        holder.descricao.text = evento.descricao
+    }
+
+    override fun getItemCount(): Int {
+        return eventos.size
+    }
+}
