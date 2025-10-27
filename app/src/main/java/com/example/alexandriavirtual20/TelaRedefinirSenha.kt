0e3db42de9dc59013ab685695a580f97314028e9
NicollyFeitosa <nicollybpcs@gmail.com>
2025-10-27 01:36:08 -0300

    Mais toasts e layout

--- app/src/main/java/com/example/alexandriavirtual20/TelaRedefinirSenha.kt
+++ app/src/main/java/com/example/alexandriavirtual20/TelaRedefinirSenha.kt
@@ -2,7 +2,9 @@ package com.example.alexandriavirtual20
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,10 @@ import androidx.core.view.WindowInsetsCompat
 class TelaRedefinirSenha : AppCompatActivity() {
 
     private lateinit var btnVoltar : ImageButton
+    private lateinit var btnRedefinirSenha: Button
+    private lateinit var senhaAtual : EditText
+    private lateinit var senha1 : EditText
+    private lateinit var senha2 : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +24,29 @@ class TelaRedefinirSenha : AppCompatActivity() {
         setContentView(R.layout.tela_redefinir_senha)
 
         btnVoltar = findViewById(R.id.botaoVoltar)
+        btnRedefinirSenha = findViewById(R.id.botaoRedefinir)
+        senhaAtual = findViewById(R.id.senhaAtual)
+        senha1 = findViewById(R.id.novaSenha)
+        senha2 = findViewById(R.id.segundaSenha)
+
         btnVoltar.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
+        }
+
+        btnRedefinirSenha.setOnClickListener {
+            val senhaAtual = senhaAtual.text.toString()
+            val novaSenha = senha1.text.toString()
+            val confirmarSenha = senha2.toString()
+
+            if (senhaAtual.isEmpty() || novaSenha.isEmpty() || confirmarSenha.isEmpty()) {
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (novaSenha != confirmarSenha) {
+                Toast.makeText(this, "Senhas não correspondem", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
         }
     }
 }
