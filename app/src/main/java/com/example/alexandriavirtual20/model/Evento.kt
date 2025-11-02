421c2d4b07133e69cb7ce35d72b44686372a2c4c
FelipeBarrosoVieiraMaciel <lipebvm@gmail.com>
2025-11-02 15:53:31 -0300

    NÃO MERGIR

--- app/src/main/java/com/example/alexandriavirtual20/model/Evento.kt
+++ app/src/main/java/com/example/alexandriavirtual20/model/Evento.kt
@@ -7,6 +7,6 @@ data class Evento (
     var horario : String,
     var descricao : String,
     var breveDescricao : String,
-    var isSelected: Boolean = false,
-    var local : String
+    var local : String,
+    var isSelected: Boolean = false
     )
