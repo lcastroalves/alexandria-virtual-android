9f6e33ba242d45bde5ec99cc04cac1c7b94a2a29
NicollyFeitosa <nicollybpcs@gmail.com>
2025-11-14 15:54:48 -0300

    Tela de cadastro integrada com o banco de dados

--- app/src/main/java/com/example/alexandriavirtual20/model/Usuario.kt
+++ app/src/main/java/com/example/alexandriavirtual20/model/Usuario.kt
@@ -1,10 +1,11 @@
 package com.example.alexandriavirtual20.model
 
 data class Usuario(
-    val id: String,
-    val nome: String,
-    val usuario: String = " ",
-    val email: String = " ",
-    val fotoPerfil: String?,
-    val senha: String = ""
+    val id: String = "",
+    val nome: String = "",
+    val usuario: String = "",
+    val email: String = "",
+    val senha: String? = "",
+    val fotoPerfil: String? = null,
+    val isAdmin: Boolean = false
 )
